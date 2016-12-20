52af885e7d1cbf122d212fabc92e7d5448e3b5b7
Chiamaka Nwolisa <chiamakanwolisa@gmail.com>
2016-12-20 20:14:44 +0100

    practise JS prototypes

---
+++ Calculator/main.js
@@ -0,0 +1,39 @@
+/**
+ * Created by chiamaka on 12/18/16.
+ */
+
+"use strict";
+
+const submitBtn = document.querySelector('button[type=submit]');
+const resultBtn = document.getElementById('results');
+const calculateField = document.getElementById('calculate');
+
+function App() {
+    this.all = [];
+};
+
+App.prototype = {
+    calculate: function (num1, num2) {
+        let sum = num1 + num2;
+        this.all.push(sum);
+        return sum;
+    },
+    allResults: function () {
+        return this.all.join("\n");
+    }
+};
+
+const calcInstance = new App();
+
+submitBtn.addEventListener('click', function () {
+    calcInstance.textContent = '';
+    const numberOne = parseInt(document.getElementById('number1').value);
+    const numberTwo = parseInt(document.getElementById('number2').value);
+    calculateField.textContent = calcInstance.calculate(numberOne, numberTwo);
+});
+
+resultBtn.addEventListener('click', () => {
+    const show = document.getElementById('showResults');
+    show.style.display = 'inline';
+    show.textContent = calcInstance.allResults();
+});
